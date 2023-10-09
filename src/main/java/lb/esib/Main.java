0dbec376f42c0769a267848f07fe9ba4fb218749
Youssef Bakouny <Youssef.Bakouny@usj.edu.lb>
2023-10-09 10:32:24 +0300

    Test Higher order functions in Java in file Main.java

--- src/main/java/lb/esib/Main.java
+++ src/main/java/lb/esib/Main.java
@@ -1,10 +1,31 @@
 package lb.esib;
 
-import static lb.esib.Functions.*;
+import java.util.function.Function;
+
+class Calculator {
+    private final int x;
+    public Calculator(int x) {
+        this.x = x;
+    }
+
+    public int add(int y) {
+        return x + y;
+    }
+}
+
 
 public class Main {
 
+    static int doOp(Function<Integer, Integer> f, Integer a) {
+        return f.apply(a);
+    }
+
+
     public static void main(String[] args) {
-        System.out.println(sumInts(0, 5));
+        Calculator add5 = new Calculator(5);
+        System.out.println(doOp(add5::add, 7));
+        System.out.println(doOp(add5::add, 10));
+
+        System.out.println("hello");
     }
 }
